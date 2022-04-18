7abbead99cb2c8528201bdbaa0ae31883eefbfc6
Nikita <oficialsmashgame@gmail.com>
2022-04-18 19:52:29 +0300

    Updates

---
+++ precache-manifest.2a899f31a1f1124628611def0d25a3ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b2d8c4d998e47d933ad4487ab013f82",
+    "url": "/React_resto_app/index.html"
+  },
+  {
+    "revision": "fc92203c1ae7a8f50c81",
+    "url": "/React_resto_app/static/css/main.a6b1eb90.chunk.css"
+  },
+  {
+    "revision": "d2a0d2e9d5543855ad3c",
+    "url": "/React_resto_app/static/js/2.2eb906e3.chunk.js"
+  },
+  {
+    "revision": "fc92203c1ae7a8f50c81",
+    "url": "/React_resto_app/static/js/main.c09dc2ae.chunk.js"
+  },
+  {
+    "revision": "c0b035f349b550e5baa5",
+    "url": "/React_resto_app/static/js/runtime~main.ebbe0bdf.js"
+  },
+  {
+    "revision": "ceb2faf089a96bede51ccde3693d012b",
+    "url": "/React_resto_app/static/media/empty-cart.ceb2faf0.ceb2faf0.png"
+  },
+  {
+    "revision": "701d6d1d57f5d528c4db1e887ff29e82",
+    "url": "/React_resto_app/static/media/shopping-cart-solid.701d6d1d.svg"
+  }
+]);
